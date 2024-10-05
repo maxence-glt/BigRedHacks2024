65264d0f4a5bd53a761097fd5a593d3ff187208c
Erik Pedersen <59267386+ezpedersen@users.noreply.github.com>
2024-10-05 18:35:02 -0400

    fixed mongoose bug with hot reloading

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,93 +4,99 @@ import { MONGO_URI } from '$env/static/private';
 
 await mongoose.connect(MONGO_URI);
 
-export const Offering = mongoose.model(
-	'Offering',
-	new mongoose.Schema(
-		{
-			_id: {
-				type: String,
-				required: true
-			},
-			owner_id: {
-				type: String,
-				required: true
-			},
-			borrower_id: {
-				type: String
-			},
-			price: {
-				type: Number,
-				required: true
-			},
-			borrow_time_limit: {
-				type: Number
-			},
-			rating: {
-				type: Number
-			},
-			created_at: {
-				type: Date,
-				required: true
-			}
-		} as const,
-		{ _id: false }
-	)
-);
-
-export const User = mongoose.model(
-	'User',
-	new mongoose.Schema(
-		{
-			_id: {
-				type: String,
-				required: true
-			},
-			username: {
-				type: String,
-				required: true
-			},
-			github_id: {
-				type: Number,
-				required: true
-			},
-			offers: [
-				{
-					type: mongoose.Schema.Types.ObjectId,
-					ref: 'Offering'
+export const Offering =
+	mongoose.models.Offering ||
+	mongoose.model(
+		'Offering',
+		new mongoose.Schema(
+			{
+				_id: {
+					type: String,
+					required: true
+				},
+				owner_id: {
+					type: String,
+					required: true
+				},
+				borrower_id: {
+					type: String
+				},
+				price: {
+					type: Number,
+					required: true
+				},
+				borrow_time_limit: {
+					type: Number
+				},
+				rating: {
+					type: Number
+				},
+				created_at: {
+					type: Date,
+					required: true
 				}
-			],
-			borrows: [
-				{
-					type: mongoose.Schema.Types.ObjectId,
-					ref: 'Offering'
-				}
-			]
-		} as const,
-		{ _id: false }
-	)
-);
+			} as const,
+			{ _id: false }
+		)
+	);
 
-export const Session = mongoose.model(
-	'Session',
-	new mongoose.Schema(
-		{
-			_id: {
-				type: String,
-				required: true
-			},
-			user_id: {
-				type: String,
-				required: true
-			},
-			expires_at: {
-				type: Date,
-				required: true
-			}
-		} as const,
-		{ _id: false }
-	)
-);
+export const User =
+	mongoose.models.User ||
+	mongoose.model(
+		'User',
+		new mongoose.Schema(
+			{
+				_id: {
+					type: String,
+					required: true
+				},
+				username: {
+					type: String,
+					required: true
+				},
+				github_id: {
+					type: Number,
+					required: true
+				},
+				offers: [
+					{
+						type: mongoose.Schema.Types.ObjectId,
+						ref: 'Offering'
+					}
+				],
+				borrows: [
+					{
+						type: mongoose.Schema.Types.ObjectId,
+						ref: 'Offering'
+					}
+				]
+			} as const,
+			{ _id: false }
+		)
+	);
+
+export const Session =
+	mongoose.models.Session ||
+	mongoose.model(
+		'Session',
+		new mongoose.Schema(
+			{
+				_id: {
+					type: String,
+					required: true
+				},
+				user_id: {
+					type: String,
+					required: true
+				},
+				expires_at: {
+					type: Date,
+					required: true
+				}
+			} as const,
+			{ _id: false }
+		)
+	);
 
 export const adapter = new MongodbAdapter(
 	mongoose.connection.collection('sessions'),
